820acc8d759113788a59425d634521409c17ba2e
Omar01~ <omarreyez31@gmail.com>
2024-11-29 16:16:53 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://omar3119.github.io/FrontedMentor---FAQ-accordion'
 })
