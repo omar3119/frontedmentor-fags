0c8a7baf8b9271a6b400fbea70e7067ea7fa07a3
Omar01~ <omarreyez31@gmail.com>
2024-11-29 17:49:24 -0500

    responsive desktop

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://omar3119.github.io/FrontedMentor---FAQ-accordion'
+  base: 'https://omar3119.github.io/frontedmentor-fags'
 })
